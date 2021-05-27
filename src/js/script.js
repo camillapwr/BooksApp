3dbb5e4bfe6dd1ddcf00b4819c90502955c2009f
Kamila Gajcy <kamila.gajcy@gmail.com>
2021-05-27 09:40:26 +0200

    Add initActions function

--- src/js/script.js
+++ src/js/script.js
@@ -8,36 +8,43 @@
     containerOf: {
       booksList: '.books-list',
     },
+    bookImage: '.book__image'
   };
   const templates = {
     booksTemplate: Handlebars.compile(document.querySelector(select.templateOf.booksTemplate).innerHTML),
   };
-
+ 
+  const booksContainer = document.querySelector(select.containerOf.booksList);//przeniosłam tu bo stosuje w obu funcjach
+  
   function render(){
-      for(let book of dataSource.books){
-          const generatedHTML = templates.booksTemplate(book);
-          const element = utils.createDOMFromHTML(generatedHTML);
-          const booksContainer = document.querySelector(select.containerOf.booksList);
-          booksContainer.appendChild(element);
+    for(let book of dataSource.books){
+      const generatedHTML = templates.booksTemplate(book);
+      const element = utils.createDOMFromHTML(generatedHTML);
 
-      }
+      booksContainer.appendChild(element);
+
     }
-render();
+  }
+  render();
 
-  }///ostatni 
+  const favoriteBooks = [];
+  console.log(favoriteBooks);
 
+  function initActions (){
+    
+    const booksImage = booksContainer.querySelectorAll(select.bookImage);
+    for(let image of booksImage){
+      image.addEventListener('dblclick', function(event){
+        event.preventDefault();
+        image.classList.add('favorite');
+        const id = image.getAttribute('data-id');
 
+        favoriteBooks.push(id);
+        console.log(favoriteBooks);
+        
+      });
+    }
+  }
+  initActions();
 
-
-
-
-
-
-
-
-
-
-
-
-
-}
+}///ostatni 

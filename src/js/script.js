55a3be520bb8ec732b8f10d74472d4ea377707e9
Kamila Gajcy <kamila.gajcy@gmail.com>
2021-05-30 11:33:38 +0200

    Add function determineRatingBgc

--- src/js/script.js
+++ src/js/script.js
@@ -18,7 +18,12 @@
   const booksContainer = document.querySelector(select.containerOf.booksList);//przeniosłam tu bo stosuje w obu funcjach
   
   function render(){
+
     for(let book of dataSource.books){
+
+      book.ratingBgc = determineRatingBgc(book.rating);
+      book.ratingWidth = book.rating * 10;
+
       const generatedHTML = templates.booksTemplate(book);
       const element = utils.createDOMFromHTML(generatedHTML);
 
@@ -90,6 +95,21 @@
         bookImage.classList.remove('hidden');
       }
     }
+  }
+  
+  function determineRatingBgc(rating) {
+    let background = '';
+
+    if(rating < 6){
+      background = 'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%);';
+    }else if (rating > 6 && rating<= 8){
+      background = 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%);';
+    }else if(rating > 8 && rating<= 9){
+      background = 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%);';
+    }else if(rating > 9){
+      background = 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%);';
+    }
+    return background;
   }
   
 }///ostatni 
